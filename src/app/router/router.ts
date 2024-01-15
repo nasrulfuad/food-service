16d00296c8af853a2bf5f6299084fded7e35a581
Nasrul Fuad <nasrulfuad08@gmail.com>
2024-01-16 00:49:21 +0700

    feat: :sparkles: add routes for findById

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -19,6 +19,10 @@ export function router(app: Express, prisma: PrismaClient): Express {
       "/toppings",
       injections.topping.controller.create.bind(injections.topping.controller) // Bind controller to use `this` instance from DI
     );
+    app.get(
+      "/toppings/:id",
+      injections.topping.controller.findById.bind(injections.topping.controller) // Bind controller to use `this` instance from DI
+    );
     app.put(
       "/toppings/:id",
       injections.topping.controller.update.bind(injections.topping.controller) // Bind controller to use `this` instance from DI
@@ -39,6 +43,10 @@ export function router(app: Express, prisma: PrismaClient): Express {
       "/fillings",
       injections.filling.controller.create.bind(injections.filling.controller) // Bind controller to use `this` instance from DI
     );
+    app.get(
+      "/fillings/:id",
+      injections.filling.controller.findById.bind(injections.filling.controller) // Bind controller to use `this` instance from DI
+    );
     app.put(
       "/fillings/:id",
       injections.filling.controller.update.bind(injections.filling.controller) // Bind controller to use `this` instance from DI
@@ -58,6 +66,10 @@ export function router(app: Express, prisma: PrismaClient): Express {
     app.post(
       "/foods",
       injections.food.controller.create.bind(injections.food.controller) // Bind controller to use `this` instance from DI
+    );
+    app.get(
+      "/foods/:id",
+      injections.food.controller.findById.bind(injections.food.controller) // Bind controller to use `this` instance from DI
     );
     app.put(
       "/foods/:id",
