249fc10e7d004846d8bc7590f25b51c3c4ec3eb1
Nasrul Fuad <nasrulfuad08@gmail.com>
2024-03-26 11:26:57 +0700

    refactor entry file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,4 @@ dotenv.config({ path: ".env" });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
